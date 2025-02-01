4c7bd59c7fb4c82fc0fcf7934d219199c1df6294
Imren <imrenrahbay@gmail.com>
2025-02-01 17:44:23 +0100

    final css

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 }
+
+const header = document.querySelector('header');
+window.addEventListener('scrooll' , () => {
+  header.classList.toggle('shadow', window.scrollY > 0 )
+})
