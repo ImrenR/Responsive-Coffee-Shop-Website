f867f36d504e99b1b3fd4742388040e5342a9791
Imren <imrenrahbay@gmail.com>
2025-02-01 18:03:51 +0100

    Final

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const search = document.querySelector('.search-box');
+let search = document.querySelector('.search-box');
 
 
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    navbar.classList.remove('active');
 }
 
-const header = document.querySelector('header');
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.toggle('active');
+}
+window.onscroll = () => {
+  navbar.classList.remove('active');
+  search.classList.remove('active');
+}
+
+let header = document.querySelector('header');
 window.addEventListener('scrooll' , () => {
   header.classList.toggle('shadow', window.scrollY > 0 )
-})
+});
